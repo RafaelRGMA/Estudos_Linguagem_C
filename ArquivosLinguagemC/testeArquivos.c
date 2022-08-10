8df6325e5601887bd2d60581bc6bcd40609e1cef
Rafael <reativacq@gmail.com>
2022-08-10 17:46:49 -0300

    Implementando função para contagem de linhas do txt

--- ArquivosLinguagemC/testeArquivos.c
+++ ArquivosLinguagemC/testeArquivos.c
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+int tamanhoTxt(FILE * palavras, int tamanhoLinha);
 
 FILE * palavras;
 char palavra[60];
 
 int main(){
-    char * resultado = (char *) malloc(sizeof(char)*60);
+    char * resultado = (char *) malloc(sizeof(char)*60);    
     palavras = fopen("palavras.txt", "r");
-    (palavras == NULL)?exit(1):printf("\narquivo aberto com sucesso\n");
-    resultado = fgets(palavra, 60, palavras);
-    printf("\nlendo a palavra: %s", palavra);
-    printf("\nOu pode ser lido desta maneira: %s\n", resultado);
+    printf("\n%i\n", tamanhoTxt(palavras, 60));
+
     fclose(palavras);
 
     return 0;
+}
+
+int tamanhoTxt(FILE * palavras, int tamanhoLinha){
+	int i = 0;
+	char linha[tamanhoLinha];
+	char linhaAnterior[tamanhoLinha];
+	while(strcmp(linha, linhaAnterior) != 0){	
+		strcpy(linhaAnterior, linha);
+		fgets(linha, tamanhoLinha, palavras);
+		if(strcmp(linha, linhaAnterior) == 0){
+			break;
+		}
+		i++;
+	}
+	return i;
 }
